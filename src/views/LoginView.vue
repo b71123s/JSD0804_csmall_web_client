5fe099ef699b4affdf7a18b89634f2c7d6e475e3
b71123s <b71123s@gmail.com>
2022-12-02 22:32:50 +0800

    111/12/02 客戶端網頁 UP01 1.創建VUE腳手架 2.創建簡易登入頁面

---
+++ src/views/LoginView.vue
@@ -0,0 +1,57 @@
+<template>
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="用戶名" prop="username">
+            <el-input v-model="ruleForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密碼" prop="password">
+            <el-input v-model="ruleForm.password"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')">登入</el-button>
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+    </el-form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                ruleForm: {
+                    username: '',
+                    password: ''
+                },
+                rules: {
+                    username: [
+                        { required: true, message: '請輸入用戶名', trigger: 'blur' },
+                        { min: 4, max: 15, message: '長度在 4 到 5 個字節', trigger: 'blur' }
+                    ],
+                    password: [
+                        { required: true, message: '請輸入密碼', trigger: 'blur' },
+                        { min: 4, max: 15, message: '長度在 4 到 5 個字節', trigger: 'blur' }
+                    ]
+                }
+            };
+        },
+        methods: {
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            },
+            resetForm(formName) {
+                this.$refs[formName].resetFields();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+/*寫CSS專用*/
+</style>
